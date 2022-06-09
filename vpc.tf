e97dc3f4f480740b96c0253cea5a4a25c5402379
Didier Gourillon <dgourillon@google.com>
2022-06-09 10:00:36 +0200

    remove network.tf

--- vpc.tf
+++ vpc.tf
@@ -4,14 +4,14 @@
 resource "google_compute_network" "vpc" {
   name                    = "${var.project_id}-vpc"
   auto_create_subnetworks = "false"
-  project   = var.project_id
+  project                 = var.project_id
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
   name          = "${var.project_id}-subnet"
   region        = var.proxy_region
-  project   = var.project_id
+  project       = var.project_id
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
 }
