468d9bc5104012ce3b24596f37ecd9f54bbb5759
Didier Gourillon <dgourillon@google.com>
2022-06-09 09:17:00 +0200

    add the elements from the kub terraform tuto - fix duplicate variables

--- gke.tf
+++ gke.tf
@@ -1,17 +1,3 @@
-variable "gke_username" {
-  default     = ""
-  description = "gke username"
-}
-
-variable "gke_password" {
-  default     = ""
-  description = "gke password"
-}
-
-variable "gke_num_nodes" {
-  default     = 2
-  description = "number of gke nodes"
-}
 
 # GKE cluster
 resource "google_container_cluster" "primary" {
